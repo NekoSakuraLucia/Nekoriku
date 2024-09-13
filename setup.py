8bd8cdc9d6f4cc88f87d3528fcfa4ee438875c2c
NekoSakuraLucia <mokurolucia@gmail.com>
2024-09-13 18:03:38 +0700

    แก้ใข setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="nekoriku",
     version="0.1",
     description="แพ็คเกจสำหรับบอทดิสคอร์ดที่มีฟีเจอร์คำสั่งสำหรับเล่นเพลงครบถ้วน",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     install_requires=[
         'discord.py',
@@ -13,5 +13,6 @@ setup(
     ],
     author="NekoSakuraLucia",
     packages=find_packages(),
+    url="https://github.com/NekoSakuraLucia/Nekoriku",
     python_requires=">=3.10"
 )
